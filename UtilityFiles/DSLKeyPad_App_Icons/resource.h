2c6a3d412a1665ad1c9fd3b9d8d90824569d4072
DemerNkardaz <92405785+DemerNkardaz@users.noreply.github.com>
2024-10-03 03:08:03 +0300

    А

--- UtilityFiles/DSLKeyPad_App_Icons/resource.h
+++ UtilityFiles/DSLKeyPad_App_Icons/resource.h
@@ -11,12 +11,13 @@
 #define IDI_ICON7                       107
 #define IDI_ICON8                       108
 #define IDI_ICON9                       109
+#define IDI_ICON10                      110
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        110
+#define _APS_NEXT_RESOURCE_VALUE        111
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
