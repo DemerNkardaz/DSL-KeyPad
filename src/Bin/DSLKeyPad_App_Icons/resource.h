f56308ac3c5ff13775611e5b5407f5c2fe0316d9
DemerNkardaz <92405785+DemerNkardaz@users.noreply.github.com>
2025-04-25 14:15:20 +0300

    А

--- src/Bin/DSLKeyPad_App_Icons/resource.h
+++ src/Bin/DSLKeyPad_App_Icons/resource.h
@@ -2,30 +2,35 @@
 // Microsoft Visual C++ generated include file.
 // Used by Resource.rc
 //
-#define IDI_ICON1                       101
-#define IDI_ICON2                       102
-#define IDI_ICON3                       103
-#define IDI_ICON4                       104
-#define IDI_ICON5                       105
-#define IDI_ICON6                       106
-#define IDI_ICON7                       107
-#define IDI_ICON8                       108
-#define IDI_ICON9                       109
-#define IDI_ICON10                      110
-#define IDI_ICON11                      112
-#define IDI_ICON12                      113
-#define IDI_ICON13                      114
-#define IDI_ICON14                      115
-#define IDI_ICON15                      117
-#define IDI_ICON16                      118
+#define IDI_APP 101
+#define IDI_NORSE 102
+#define IDI_GLAGOLITIC 103
+#define IDI_TURKIC 104
+#define IDI_PERMIC 105
+#define IDI_HUNGARIAN 106
+#define IDI_GOTHIC 107
+#define IDI_IPA 108
+#define IDI_DISABLED 109
+#define IDI_MATH 110
+#define IDI_VIET 111
+#define IDI_PINYIN 112
+#define IDI_ITALIC 113
+#define IDI_PHOENICIAN 114
+#define IDI_SOUTH_ARABIAN 115
+#define IDI_NORTH_ARABIAN 116
+#define IDI_CARIAN 117
+#define IDI_LYCIAN 118
+#define IDI_TIFINAGH 119
+#define IDI_UGARITIC 120
+#define IDI_PERSIAN 121
 
 // Next default values for new objects
-// 
+//
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        119
-#define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
-#define _APS_NEXT_SYMED_VALUE           101
+#define _APS_NEXT_RESOURCE_VALUE 119
+#define _APS_NEXT_COMMAND_VALUE 40001
+#define _APS_NEXT_CONTROL_VALUE 1001
+#define _APS_NEXT_SYMED_VALUE 101
 #endif
 #endif
