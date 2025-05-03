7f1c2b4b3d6308a13a7c5997525e8f86d3c98b5c
DemerNkardaz <92405785+DemerNkardaz@users.noreply.github.com>
2025-05-03 23:44:15 +0300

    Test of try to multiline release body text

--- src/workflow.ps1
+++ src/workflow.ps1
@@ -2,7 +2,34 @@
 $preRelease = "True"
 $title = "DSL KeyPad — V[0.1.1.2-alpha-testing] — (αλφα)"
 $message =@"
-Automatically created/updated via build process in workflow.<br>\n\n<table>\n		<tr>\n			<td><b>Stamp</b></td>\n			<td>\n				2025-05-03 23:36:21\n			</td>\n		</tr>\n		<tr>\n			<td><b>Version</b></td>\n			<td>\n				&#128229;&emsp14;<a href="https://github.com/DemerNkardaz/DSL-KeyPad/releases/download/0.1.1.2-alpha-testing/DSL-KeyPad-0.1.1.2-alpha-testing.zip">0.1.1.2-alpha-testing</a>\n			</td>\n		</tr>\n		<tr>\n			<td><b>Requirements</b></td>\n			<td>\n				<img src="https://www.autohotkey.com/favicon.ico" width="16" style="vertical-align:middle;">&emsp14;<a href="https://www.autohotkey.com/download/ahk-v2.exe">AutoHotkey v2.^</a>\n			</td>\n		</tr>\n</table>\n\n**Versions compare** [0.1.1.1-alpha-testing&ensp;>>>&ensp;0.1.1.2-alpha-testing](https://github.com/DemerNkardaz/DSL-KeyPad/compare/0.1.1.1-alpha-testing...0.1.1.2-alpha-testing#files_bucket) | [Changelog](https://github.com/DemerNkardaz/DSL-KeyPad/tree/main/CHANGELOG.md)\n\n[Yalla Nkardaz’s custom files](https://github.com/DemerNkardaz/DSL-KeyPad-Custom-Files) repository for DSL KeyPad.\n\n@DemerNkardaz 
+Automatically created/updated via build process in workflow.<br>
+
+<table>
+		<tr>
+			<td><b>Stamp</b></td>
+			<td>
+				2025-05-03 23:44:01
+			</td>
+		</tr>
+		<tr>
+			<td><b>Version</b></td>
+			<td>
+				&#128229;&emsp14;<a href="https://github.com/DemerNkardaz/DSL-KeyPad/releases/download/0.1.1.2-alpha-testing/DSL-KeyPad-0.1.1.2-alpha-testing.zip">0.1.1.2-alpha-testing</a>
+			</td>
+		</tr>
+		<tr>
+			<td><b>Requirements</b></td>
+			<td>
+				<img src="https://www.autohotkey.com/favicon.ico" width="16" style="vertical-align:middle;">&emsp14;<a href="https://www.autohotkey.com/download/ahk-v2.exe">AutoHotkey v2.^</a>
+			</td>
+		</tr>
+</table>
+
+**Versions compare** [0.1.1.1-alpha-testing&ensp;>>>&ensp;0.1.1.2-alpha-testing](https://github.com/DemerNkardaz/DSL-KeyPad/compare/0.1.1.1-alpha-testing...0.1.1.2-alpha-testing#files_bucket) | [Changelog](https://github.com/DemerNkardaz/DSL-KeyPad/tree/main/CHANGELOG.md)
+
+[Yalla Nkardaz’s custom files](https://github.com/DemerNkardaz/DSL-KeyPad-Custom-Files) repository for DSL KeyPad.
+
+@DemerNkardaz 
 "@
 
 Write-Host "Version: $ver"
